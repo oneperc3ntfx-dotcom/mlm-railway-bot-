b73f56468ea1868bd42c730fec81f419a1e55084
oneperc3ntfx-dotcom <oneperc3ntfx@gmail.com>
2026-06-26 06:01:06 +0700

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ load_dotenv()
 GOOGLE_SHEET_ID = os.getenv("GOOGLE_SHEET_ID")
 
 DATA_SHEET = "data"
-OWNER_SHEET = "OWNER"
+OWNER_SHEET = "yuri"
 CLASS1_SHEET = "CLASS_1"
 CLASS2_SHEET = "CLASS_2"
 
