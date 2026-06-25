a22670c0ebc3eb7a3cfd98cb7cd85a2b9f449d14
oneperc3ntfx-dotcom <oneperc3ntfx@gmail.com>
2026-06-26 05:31:17 +0700

    Update config.py

--- config.py
+++ config.py
@@ -4,9 +4,14 @@ from dotenv import load_dotenv
 load_dotenv()
 
 GOOGLE_SHEET_ID = os.getenv("GOOGLE_SHEET_ID")
-OWNER_SHEET_ID = os.getenv("OWNER_SHEET_ID")
-CLASS1_SHEET_ID = os.getenv("CLASS1_SHEET_ID")
-CLASS2_SHEET_ID = os.getenv("CLASS2_SHEET_ID")
+
+DATA_SHEET = "data!A:G"
+DB_SHEET = "owner_db!A:D"
+
+OWNER_SHEET = "owner!A:D"
+CLASS1_SHEET = "class_1!A:E"
+CLASS2_SHEET = "class_2!A:D"
 
 SERVICE_EMAIL = os.getenv("GOOGLE_SERVICE_ACCOUNT_EMAIL")
+
 PRIVATE_KEY = os.getenv("GOOGLE_PRIVATE_KEY").replace("\\n", "\n")
