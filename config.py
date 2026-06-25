7f4b4a5b83f0056955a8fb82dd0030ad61cb926a
oneperc3ntfx-dotcom <oneperc3ntfx@gmail.com>
2026-06-26 05:37:59 +0700

    Update config.py

--- config.py
+++ config.py
@@ -5,13 +5,16 @@ load_dotenv()
 
 GOOGLE_SHEET_ID = os.getenv("GOOGLE_SHEET_ID")
 
-DATA_SHEET = "data!A:G"
-DB_SHEET = "owner_db!A:D"
+# TRANSAKSI
+DATA_SHEET = "'data'!A:G"
 
-OWNER_SHEET = "owner!A:D"
-CLASS1_SHEET = "class_1!A:E"
-CLASS2_SHEET = "class_2!A:D"
+# DB REFERRAL (SHEET OWNER)
+DB_SHEET = "'OWNER'!A:D"
+
+# OUTPUT SHEETS
+OWNER_SHEET = "'OWNER_REPORT'!A:D"
+CLASS1_SHEET = "'CLASS_1'!A:E"
+CLASS2_SHEET = "'CLASS_2'!A:D"
 
 SERVICE_EMAIL = os.getenv("GOOGLE_SERVICE_ACCOUNT_EMAIL")
-
 PRIVATE_KEY = os.getenv("GOOGLE_PRIVATE_KEY").replace("\\n", "\n")
